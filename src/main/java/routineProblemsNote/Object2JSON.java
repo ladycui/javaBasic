ad3d021c00c3b4a67ec1e7edcb910bb6ff143a9c
ladycui <lady_cui@126.com>
2019-12-18 20:45:33 +0800

    normal obj 2 json object

---
+++ src/main/java/routineProblemsNote/Object2JSON.java
@@ -0,0 +1,21 @@
+package routineProblemsNote;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * @Auther: cyn
+ * @Date: 2019-12-18 20:41
+ * @Description: normal obj can be transferred to JSON object
+ */
+public class Object2JSON {
+    public static void main(String[] args) {
+        Source source = new Source();
+        source.setAddr("1.1.1.1");
+        source.setPort(80);
+        source.setWeight(1);
+
+        JSONObject jsonObject = (JSONObject) JSON.toJSON(source);
+
+    }
+}
---
+++ src/main/java/routineProblemsNote/Source.java
@@ -0,0 +1,17 @@
+package routineProblemsNote;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Auther: cyn
+ * @Date: 2019-06-05 14:11
+ * @Description:
+ */
+@Data
+@NoArgsConstructor
+public class Source {
+    int port;
+    int weight;
+    String addr;
+}
